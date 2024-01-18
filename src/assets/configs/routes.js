cdf01decfbd44df69caef5dccb50386d2197eae0
Dinoin <xji43399@gmail.com>
2024-01-19 00:00:04 +0800

    feat: Create FlipPage in ShowCase and configure the routes for show case infomation with the CodeSandbox version.

---
+++ src/assets/configs/routes.js
@@ -0,0 +1,93 @@
+export const config = [
+    {
+      title: "Draw circle background",
+      url: "https://mw49ts.csb.app/",
+      img: require(`assets/images/mw49ts.png`),
+    },
+    {
+      title: "Promise + flag",
+      url: "https://dpztwz.csb.app/",
+      img: require(`assets/images/dpztwz.png`),
+    },
+    {
+      title: "Exceljs: Download exl",
+      url: "https://fwvml5.csb.app/",
+      img: require(`assets/images/fwvml5.png`),
+    },
+    {
+      title: "class - 耗時計算",
+      url: "https://dqr77d.csb.app/",
+      img: require(`assets/images/dqr77d.png`),
+    },
+    {
+      title: "多input取值",
+      url: "https://lpwx4w.csb.app/",
+      img: require(`assets/images/lpwx4w.png`),
+    },
+    {
+      title: "HashRouter with unified layout.",
+      url: "https://h9rc67.csb.app/",
+      img: require(`assets/images/h9rc67.png`),
+    },
+    {
+      title: "JS的Regexp演示",
+      url: "https://sxxm2v.csb.app/",
+      img: require(`assets/images/sxxm2v.png`),
+    },
+    {
+      title: "MUI: Button text line break with subtext.",
+      url: "https://jkyfml.csb.app/",
+      img: require(`assets/images/jkyfml.png`),
+    },
+    {
+      title: "MUI: 限定範圍的Backdrop(遮罩)",
+      url: "https://rxr24r.csb.app/",
+      img: require(`assets/images/rxr24r.png`),
+    },
+    {
+      title: "MUI: RadioGroup horizontal arrangement.",
+      url: "https://7y3g57.csb.app/",
+      img: require(`assets/images/7y3g57.png`),
+    },
+    {
+      title: "MUI: Card(Flexbox的子元素靠右)",
+      url: "https://ny8tjn.csb.app/",
+      img: require(`assets/images/ny8tjn.png`),
+    },
+    {
+      title: "MUI: Autocomplete模糊查詢支持",
+      url: "https://3nyj5t.csb.app",
+      img: require(`assets/images/3nyj5t.png`),
+    },
+    {
+      title: "MUI: TextField+BT to be select",
+      url: "https://r33yzn.csb.app/",
+      img: require(`assets/images/r33yzn.png`),
+    },
+    {
+      title: "MUI+AgGrid: TransferAgGrid",
+      url: "https://tf48th.csb.app/",
+      img: require(`assets/images/tf48th.png`),
+    },
+    {
+      title: "Ag-Grid: 自訂header顯示",
+      url: "https://dx86cp.csb.app/",
+      img: require(`assets/images/dx86cp.png`),
+    },
+    {
+      title: "Ag-Grid: 時間格式資料(valueFormatter & filter)",
+      url: "https://xqn5qv.csb.app/",
+      img: require(`assets/images/xqn5qv.png`),
+    },
+    {
+      title: "Plotly - bar: basic.",
+      url: "https://pl7w4q.csb.app/",
+      img: require(`assets/images/pl7w4q.png`),
+    },
+    // {
+    //   title: "x",
+    //   url: "x",
+    //   img: require(`assets/images/rxr24r.png`),
+    // },
+  ];
+  
--- src/pages/ShowCase.js
+++ src/pages/ShowCase.js
@@ -1,7 +1,72 @@
-export default function ShowCase(){
-    return (
-        <div>
-            <h1>ShowCase</h1>
-        </div>
-    )
+import { useState, useEffect, useRef } from "react";
+import FlipPage from "react-flip-page";
+
+import { config } from "assets/configs/routes";
+
+import "assets/css/ShowCase.css";
+
+export default function ShowCase() {
+  const ref_FlipBackground = useRef();
+
+  const [flipPageWidth, setFlipPageWidth] = useState("");
+  const [flipPageHeight, setFlipPageHeight] = useState("");
+
+  useEffect(() => {
+    console.log(ref_FlipBackground.current.clientHeight);
+    console.log(ref_FlipBackground.current.clientWidth);
+    //49:26
+    const h = Math.floor(
+      (ref_FlipBackground.current.clientHeight / 100) * 80 * 0.9
+    );
+    const w_withH = Math.floor((h / 26) * 49);
+    console.log("(withH)w:h=>" + w_withH + ":" + h + ` =>${h / w_withH}`);
+    const w = ref_FlipBackground.current.clientWidth * 0.9;
+    const h_withW = Math.floor((w / 49) * 26);
+    console.log("(withW)w:h=>" + w + ":" + h_withW + ` =>${h_withW / w}`);
+    if (h < h_withW) {
+      setFlipPageHeight(h);
+      setFlipPageWidth(w_withH);
+    } else {
+      setFlipPageHeight(h_withW);
+      setFlipPageWidth(w);
+    }
+  }, []);
+
+  return (
+    <div
+      className="page-background"
+      //   style={{ backgroundImage: "url(`./images/BookWell.jpg`)" }}
+    >
+      <div
+        className="flip-background"
+        // style={{ backgroundImage: "url(`./images/OpenBook.png`)" }}
+        ref={ref_FlipBackground}
+      >
+        <FlipPage
+          orientation="horizontal" // 設置翻頁的方向，可以是 'horizontal' 或 'vertical'
+          uncutPages // 開啟此選項以顯示未分割的頁面，模擬書本的翻頁效果
+          height={flipPageHeight}
+          width={flipPageWidth}
+          style={{ margin: "auto" }}
+        >
+          <article>
+            <h1>Show Cases</h1>
+            <p>請翻動書頁查看</p>
+          </article>
+          {config.map((v, i) => {
+            return (
+              <article key={`page_${i}`}>
+                <h1>{v.title}</h1>
+                {v.directions ? <p>{v.directions}</p> : ""}
+                <a href={v.url} target="_blank" style={{ overflowY: "hidden" }}>
+                  {/* GO➢ */}
+                  <img src={v.img} style={{ width: "100%" }} />
+                </a>
+              </article>
+            );
+          })}
+        </FlipPage>
+      </div>
+    </div>
+  );
 }
